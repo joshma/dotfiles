9674a6edde9f6d3d9925bc173001f9929fc16210
Joshua Ma <me@joshma.com>
2021-08-14 19:51:31 -0700

    Disable overwriting zshrc for now

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ ln -s /usr/local/bin/diffsofancy/diff-so-fancy /usr/local/bin/diff-so-fancy
 
 # Copy configuration files
 cp -r . $DOTFILES
-ln -sf $DOTFILES/zshrc ~/.zshrc
+# ln -sf $DOTFILES/zshrc ~/.zshrc
 ln -sf $DOTFILES/vim ~/.vim
 ln -sf $DOTFILES/vimrc ~/.vimrc
 ln -sf $DOTFILES/gitconfig ~/.gitconfig
