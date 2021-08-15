af58805e9110ccaf873772caca5e3734557b0ebf
Joshua Ma <me@joshma.com>
2021-08-15 11:37:22 -0700

    Update GOPATH

--- path.zsh
+++ path.zsh
@@ -1,9 +1,7 @@
 export PATH=/usr/local/bin:$PATH
 
 # Go
-if [[ $CODESPACES == "true" ]]; then
-  export GOPATH=/go
-else
+if [[ $CODESPACES != "true" ]]; then
   export GOPATH=$HOME/go
 fi
 export PATH=$PATH:$GOPATH/bin
