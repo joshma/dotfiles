16635290d3459fe84be1932ebf3b97c139c9afa4
Joshua Ma <me@joshma.com>
2021-08-14 21:56:36 -0700

    Use sudo

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 DOTFILES=$HOME/dotfiles
 
 # Install diff-so-fancy
-git clone https://github.com/so-fancy/diff-so-fancy /usr/local/bin/diffsofancy
-ln -s /usr/local/bin/diffsofancy/diff-so-fancy /usr/local/bin/diff-so-fancy
+sudo git clone https://github.com/so-fancy/diff-so-fancy /usr/local/bin/diffsofancy
+sudo chown -R "${whoami}:${whoami}" /usr/local/bin/diffsofancy
+sudo ln -s /usr/local/bin/diffsofancy/diff-so-fancy /usr/local/bin/diff-so-fancy
 
 # Copy configuration files
 cp -r . $DOTFILES
